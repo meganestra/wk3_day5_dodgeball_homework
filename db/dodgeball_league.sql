8883bb0e62f1dbe6b7fb4cdf0f7e6971183512a8
Megan Strachan <m.e.strachan@gmail.com>
2016-06-24 09:31:53 +0100

    initial commit

---
+++ db/dodgeball_league.sql
@@ -0,0 +1,16 @@
+DROP TABLE matches;
+DROP TABLE teams;
+
+CREATE TABLE teams (
+  id SERIAL4 primary key,
+  name VARCHAR(255) not null
+);
+
+CREATE TABLE matches (
+  id SERIAL4 primary key,
+  home_team_id INT4 references teams(id),
+  away_team_id INT4 references teams(id),
+  home_team_score INT2,
+  away_team_score INT2
+);
+
