e950f5e23af355adf661f29eed269e6b0f52337d
Megan Strachan <m.e.strachan@gmail.com>
2016-06-24 11:56:05 +0100

    add save method to match

--- console.rb
+++ console.rb
@@ -10,8 +10,8 @@ t1 = team1.save()
 team2 = Team.new({"name" => "Average Joes"}, runner)
 t2 = team2.save()
 
-match1 = Match.new({"home_team_id" => team1.id, "away_team_id" => team2.id, "home_team_score" => 10, "away_team_score" => 20}, runner)
-
+match1 = Match.new({"home_team_id" => t1.id, "away_team_id" => t2.id, "home_team_score" => 10, "away_team_score" => 20}, runner)
+m1 = match1.save()
 
 
 
--- models/match.rb
+++ models/match.rb
@@ -1,5 +1,6 @@
 require_relative('sql_runner')
 require_relative('team')
+require('pg')
 
 class Match
 
@@ -7,11 +8,27 @@ class Match
 
   def initialize(options, runner)
     @id = options['id'].to_i
-    @home_team_id = options['home_team_id'].to_i
-    @away_team_id = options['away_team_id'].to_i
-    @home_team_score = options['home_team_score'].to_i
-    @away_team_score = options['away_team_score'].to_i
+    @home_team_id = options['home_team_id']
+    @away_team_id = options['away_team_id']
+    @home_team_score = options['home_team_score']
+    @away_team_score = options['away_team_score']
     @runner = runner
+  end
+
+  def save()
+    sql = "INSERT INTO matches (home_team_id, away_team_id, home_team_score, away_team_score) VALUES (#{@home_team_id}, #{@away_team_id}, #{@home_team_score}, #{@away_team_score}) RETURNING *"
+    return Match.map_item(sql, @runner)
+  end
+
+  def self.map_items(sql, runner)
+    matches_data = runner.run(sql)
+    result = matches_data.map { |match| Match.new(match, runner) }
+    return result
+  end
+
+  def self.map_item(sql, runner)
+    result = Match.map_items(sql, runner)
+    return result.first
   end
 
 
